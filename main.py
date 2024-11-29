db43a77148824063f139fdd87f55c99c96e17690
Petar <petar.kovacovski@gmail.com>
2024-11-29 18:13:52 +0100

    Shooting implemented

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 from asteroid import Asteroid
+from shot import Shot
 
 def main():
     pygame.init()
@@ -14,11 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
     
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -38,13 +40,21 @@ def main():
         screen.fill("black")
         
         for obj in asteroids:
+            for s in shots:
+                if s.collides_with(obj):
+                    obj.kill()
+                    s.kill()
+            
             if obj.collides_with(player):
                 print("Game over!")
                 sys.exit()
 
+
         
         for obj in drawable:
             obj.draw(screen)
+            
+
         
         pygame.display.flip()
         dt = clock.tick(60) / 1000
